b4636f2ef41a27e2877412cf304031aa9e04b9fc
Illeprih <58072285+Illeprih@users.noreply.github.com>
2019-12-15 11:56:52 +0100

    Edited globals to support extra options

---
+++ Globals.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dragoon_Modifier {
+    public class Globals {
+        public static ushort HOTKEY = 0;
+        public static byte DISC = 1;
+        public static byte CHAPTER = 0;
+        public static int BATTLE_VALUE = 0;
+        public static ushort ENCOUNTER_ID = 0;
+        public static ushort MAP = 0;
+        public static byte[] PARTY_SLOT = new byte[3];
+        public static byte DRAGOON_SPIRITS = 0;
+        public static bool IN_BATTLE = false;
+        public static bool STATS_CHANGED = false;
+        public static dynamic BATTLE = new System.Dynamic.ExpandoObject();
+        public static dynamic DICTIONARY = new System.Dynamic.ExpandoObject();
+        public static bool FIRST_RUN = true;
+        public static bool MONSTER_CHANGE = false;
+        public static bool DROP_CHANGE = false;
+        public static bool DROP_DEFINED = true;
+        public static string MOD = "US_Base";
+    }
+}
